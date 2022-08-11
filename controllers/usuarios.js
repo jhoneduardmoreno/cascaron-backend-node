82ae65972697b50ce8bb6b72db101250d03ae0f3
Jhon Moreno <jhoneduar1994@hotmail.com>
2022-08-10 20:38:44 -0500

    Cascaron para backend con node

---
+++ controllers/usuarios.js
@@ -0,0 +1,60 @@
+const { response, request } = require('express');
+
+
+const usuariosGet = (req = request, res = response) => {
+
+    const { q, nombre, apikey} = req.query;
+
+    res.json({
+        "msg": "get API - controlador",
+        q,
+        nombre,
+        apikey
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+
+    const id = req.params.id;
+
+    res.json({
+        "msg": "put API - controlador",
+        id
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+
+    const {nombre, edad} = req.body;
+
+    res.status(201).json({
+        "msg": "post API - controlador",
+        nombre, 
+        edad
+    });
+
+}
+
+const usuariosDelete = (req, res = response) => {
+
+    res.json({
+        "msg": "delete API - controlador"
+    });
+
+}
+
+const usuariosPatch = (req, res = response) => {
+
+    res.json({
+        "msg": "patch API - controlador"
+    });
+
+}
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete,
+    usuariosPatch
+}
